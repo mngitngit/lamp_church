6693d833420886ba2aceec2e4ea078e591a00fb5
Melanie Ngitngit <melanie.ngitngit@smartchartsfx.com>
2022-10-13 00:49:00 +0800

    shell file

---
+++ update-production.sh
@@ -0,0 +1,21 @@
+git pull origin master
+
+npm run prod
+
+rm ../public_html/js/app.js
+rm ../public_html/js/registration.js
+rm ../public_html/js/payment.js
+rm ../public_html/js/activities.js
+rm ../public_html/js/booking.js
+
+mv public/js/app.js ../public_html/js
+mv public/js/registration.js ../public_html/js
+mv public/js/payment.js ../public_html/js
+mv public/js/activities.js ../public_html/js
+mv public/js/booking.js ../public_html/js
+
+php artisan config:clear
+
+php artisan cache:clear
+
+php artisan config:cache
