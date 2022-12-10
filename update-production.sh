bafa388bbe3c0bd2ff3dca3dedac62a43c8c85ae
mngitngit <melanie.ngitngit@smartchartsfx.com>
2022-12-10 19:23:58 +0800

    attendance

--- update-production.sh
+++ update-production.sh
@@ -5,12 +5,14 @@ git pull origin master
 npm run prod
 
 rm ../public_html/js/app.js
+rm ../public_html/js/activities.js
 rm ../public_html/js/registration.js
 rm ../public_html/js/payment.js
 rm ../public_html/js/activities.js
 rm ../public_html/js/booking.js
 
 mv public/js/app.js ../public_html/js
+rm public/js/activities.js ../public_html/js
 mv public/js/registration.js ../public_html/js
 mv public/js/payment.js ../public_html/js
 mv public/js/activities.js ../public_html/js
