46b2a473d946e655ced8d7fe929592fe8892dd8a
mngitngit <melanie.ngitngit@smartchartsfx.com>
2022-12-27 12:43:19 +0800

    ticket adjustments for attendance

--- resources/views/attendance/index.blade.php
+++ resources/views/attendance/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('scripts')
-<script src="{{ asset('js/attendance.js') }}" defer></script>
+<script src="{{ asset('js/attendance.js?time=') }}{{ time() }}" defer></script>
 @endsection
 
 @section('content')
