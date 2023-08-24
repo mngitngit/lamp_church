d010ad0dd0a44ab2dd93bfa7fd9a3271e72ffa6b
mngitngit <melanie.ngitngit@smartchartsfx.com>
2023-08-24 18:43:00 +0800

    modified some wordings

--- public/js/booking.js
+++ public/js/booking.js
@@ -7333,15 +7333,15 @@ __webpack_require__.r(__webpack_exports__);
     },
     open: function open() {
       var msg = '<strong>Congratulations!</strong> Your registration has been accepted! ';
-      if (this.registrations[0].registration_type === 'Member' && this.registrations[0].attending_option === 'Hybrid') msg += '<br /><br /><small>Please settle your balance or at least pay partially to confirm your booking. If your booking is left unsettled within 7 days, it will be canceled automatically.<br /><br />If your booking is cancelled, please reach out to your local coordinator for assistance.</small>';
+      if (this.registrations[0].registration_type === 'Member' && this.registrations[0].attending_option === 'Hybrid') msg += '<br /><br /><small>Please settle your balance or at least pay partially to confirm your booking. If your booking is left unsettled within 7 days, it will be cancelled automatically.<br /><br />If your booking is cancelled, please reach out to your local coordinator for assistance.</small>';
       if (this.registrations[0].attending_option === 'Online') msg += '<br /><br /><small>To watch the live broadcast, join our FB Group <br/><a href="https://www.facebook.com/groups/446318280091482">https://www.facebook.com/groups/446318280091482</a></small>';
-      if (this.registrations[0].registration_type === 'Member') msg += '<br /><br /><small>Note: <i>A new LAMP ID Number is issued for you.</i> If you wish to replace your old AWTA Card with the new LAMP ID, please contact your local coordinator and pay 35 pesos for the issuance.</small><br/><br/><img width="170" height="100" class="mb-3 rounded shadow" src="/images/new_id.jpg">';
+      if (this.registrations[0].registration_type === 'Member') msg += '<br /><br /><small>Note: <i>A new LAMP ID Number is issued for you.</i> If you wish to replace your old AWTA Card with the new LAMP ID, please contact your local coordinator and pay 35 pesos for the issuance.</small><br/><br/><img width="170" height="100" class="mb-3 rounded shadow" src="/images/new_id.jpg"><br/><small>sample ID only</small>';
       this.$confirm(msg, 'You did it!', {
-        confirmButtonText: 'OK',
-        type: 'success',
+        confirmButtonText: 'Close',
         showCancelButton: false,
         width: '50%',
-        showClose: false,
+        showClose: true,
+        type: 'success',
         center: true,
         dangerouslyUseHTMLString: true
       });
@@ -7876,7 +7876,7 @@ var render = function render() {
       staticClass: "row"
     }, [_c("div", {
       staticClass: "col-md-12"
-    }, [_c("small", [_vm._v("*** Please screenshot this ticket. This is will be your gate pass to the event place if your LAMP ID/Guest Ticket is lost.")])])]) : _vm._e()])])], 1);
+    }, [_c("small", [_vm._v("***Please screenshot this ticket. This will be your virtual LAMP ID number, in case you opted not to avail the physical card. To be use in future LAMP church events/activities.")])])]) : _vm._e()])])], 1);
   }), 0);
 };
 
