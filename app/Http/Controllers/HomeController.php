fe1ebd0f57205bb4610bf875b654bceb355614e5
mngitngit <melanie.ngitngit@smartchartsfx.com>
2023-08-30 23:26:43 +0800

    mail adjustment

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,6 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
-        $this->notify(3);
         $registration = Registration::withSum('payments', 'amount');
 
         if ($request->type && $request->type === 'registration') {
