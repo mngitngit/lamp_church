7783fea5afe48dd2368e607412b26d612b5ba16a
mngitngit <melanie.ngitngit@smartchartsfx.com>
2023-09-01 00:08:00 +0800

    modified email notif

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,8 +23,6 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
-        $this->notify(4);
-
         $registration = Registration::withSum('payments', 'amount');
 
         if ($request->type && $request->type === 'registration') {
