17fd58620970c78733187e5b228c11d4e65aef95
mngitngit <melanie.ngitngit@smartchartsfx.com>
2023-12-19 16:26:17 +0800

    tagging if received HG

--- app/Http/Controllers/ReceivedHGController.php
+++ app/Http/Controllers/ReceivedHGController.php
@@ -38,7 +38,7 @@ class ReceivedHGController extends Controller
             return response()->json(['error' => 'LAMP ID/Guest code is required.'], 422);
         }
 
-        $registration = Registration::select('uuid', 'email', 'fullname', 'registration_type', 'local_church', 'cluster_group', 'country', 'attending_option')->where('uuid', $uuid)->first();
+        $registration = Registration::where('uuid', $uuid)->first();
 
         if (!$registration) {
             return response()->json(['error' => 'Delegate not found.'], 422);
