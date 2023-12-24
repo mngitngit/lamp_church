2907b47f69f82cb2dffa996ddda1a8e9e6f67c49
mngitngit <melanie.ngitngit@smartchartsfx.com>
2023-12-25 01:31:01 +0800

    reg form

--- routes/console.php
+++ routes/console.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Notification;
 */
 Artisan::command('send-out-event-reminder', function () {
     $this->comment('---------------------------------- ' . \Carbon\Carbon::today() . ' ---------------------------------');
-    $registrations = Registration::where('attending_option', AttendingOption::Hybrid)->where('id', '>', 309)->get();
+    $registrations = Registration::where('attending_option', AttendingOption::Hybrid)->where('id', '>', 396)->get();
     
     foreach ($registrations as $registration) {
         if ($registration->email != '') {
