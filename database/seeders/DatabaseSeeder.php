9dc171f8700ee651050f7ceab3f25e8ac5c8eda5
mngitngit <melanie.ngitngit@yahoo.com>
2024-08-24 02:38:49 +0800

    removed lookup on seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,6 @@ class DatabaseSeeder extends Seeder
             UserTableSeeder::class,
             PermissionsTableSeeder::class,
             SlotsTableSeeder::class,
-            LookUpTableSeeder::class,
             AvailableUUIDsTableSeeder::class
         ]);
     }
