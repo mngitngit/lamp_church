45f3cec4ced6f8d1da907974d3600e0521eaf4c2
mngitngit <melanie.ngitngit@yahoo.com>
2024-08-27 18:04:12 +0800

    lookup seeder

---
+++ add on lookup:.rb
@@ -0,0 +1,27 @@
+add on lookup:
+- cluster
+- availed_new_lamp_id
+
+move to registration additional data:
+- visitor_to_member
+- medical_assistance_needed
+
+- replace with_awta_card with with_lamp_id
+
+
+done:
+configurable zoom details - done
+configurable due dates on email templates - done
+display disclosure pop up and make the date configurable - done
+update the prompt message "A new lamp ID..." - done
+update payment dates and make it configurable - done
+change AWTA Card number field label into LAMP ID number - done
+display zoom details on success page for online attendees - done
+
+pending:
+update cluster groups
+make the fb name mandatory field
+unrequire the cluster group field
+investigate the manage booking (not working) - unreplicable on my end
+update the design for registration site
+group registration for online guests
